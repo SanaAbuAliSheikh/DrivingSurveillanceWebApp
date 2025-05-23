ffda2c6cf79824c88b2c6fb9ac395defc6d3a8ed
Sana-Abuali <sana@smartenergypay.com>
2025-05-23 03:49:26 +0200

    add comment

--- api/dnn.ts
+++ api/dnn.ts
@@ -1,3 +1,4 @@
+// API
 export async function getDnnModels(limit:number, offset:number, search:string) {
   const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:"+process.env.PORT;
   const res = await fetch(`${baseUrl}/api/v1/dnn?limit=${limit}&offset=${offset}&search=${search}`, {
